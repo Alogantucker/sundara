e2dc45f2050069706db97d73f8cb7dd3fc4933b1
sudo-thoth <ALoganTucker@gmail.com>
2023-09-07 18:42:05 -0400

    add google sheets api

---
+++ app.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+
+const apiKey = process.env.Google_API_KEY;
+const clientID = process.env.Google_CLIENT_ID;
+const spreadsheetID = process.env.Google_Sundara_Email_List_SPREADSHEET_ID;
+
+
+const form = document.querySelector('#newsletter-form');
+const emailInput = document.querySelector('#email-input');
+
+// Initialize the Google Sheets API
+gapi.load('client', init);
+
+async function init() {
+    try {
+        await gapi.client.init({
+            apiKey: `${apiKey}`, // Your Google API key
+            discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+            clientId: `${clientID}`, // Your Google API client ID
+            scope: 'https://www.googleapis.com/auth/spreadsheets',
+        });
+    } catch (error) {
+        console.error('Google API initialization error:', error);
+    }
+}
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const email = emailInput.value;
+
+    try {
+        const response = await gapi.client.sheets.spreadsheets.values.append({
+            spreadsheetId: `${spreadsheetID}`, // Replace with your Google Sheet ID
+            range: 'A:A', // Specify the entire A column
+            valueInputOption: 'RAW',
+            insertDataOption: 'INSERT_ROWS',
+            values: [[email]],
+        });
+
+        if (response.status === 200) {
+            // Success, handle accordingly
+            console.log('Email added to Google Sheet successfully');
+        } else {
+            // Handle error
+            console.error('Error adding email to Google Sheet:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Google Sheets API request error:', error);
+    }
+});
